99d8fa35d030d8a3c20b465346b3a113102ae18a
Furkan yagci <furkanyagcify@gmail.com>
2022-01-11 22:49:01 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,57 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id+" - "+car.BradnId +" - " + car.CategoryId +" - " +car.ColorId +" - " + car.ModelYear + " - " +car.Description+" - "+ car.DailyPrice);
+            }
+
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+            Car car1 = new Car { Id = 5, BradnId = 98, CategoryId = 1, ColorId = 30, DailyPrice = 1250, Description = "Station Wagon otomobil", ModelYear = 2022 };
+            carManager.Add(car1);
+
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+            carManager.Delete(car1);
+
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+            foreach (var car in carManager.GetAllByCategory(2))
+            {
+                Console.WriteLine("GetAllByCategory ile çağırılan : " + car.Id + " - " + car.BradnId + " - " + car.CategoryId + " - " + car.ColorId + " - " + car.ModelYear + " - " + car.Description + " - " + car.DailyPrice);
+            }
+
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+            foreach (var car in carManager.GetById(2))
+            {
+                Console.WriteLine("GetById ile çağırılan : " + car.Id + " - " + car.BradnId + " - " + car.CategoryId + " - " + car.ColorId + " - " + car.ModelYear + " - " + car.Description + " - " + car.DailyPrice);
+            }
+
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+            Car car2 = new Car { Id = 1, BradnId = 98, CategoryId = 7, ColorId = 30, DailyPrice = 999, Description = "Sport sedan otomobil", ModelYear = 2023 };
+
+            carManager.Update(car2);
+
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id + " - " + car.BradnId + " - " + car.CategoryId + " - " + car.ColorId + " - " + car.ModelYear + " - " + car.Description + " - " + car.DailyPrice);
+            }
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _car;
+
+        public InMemoryCarDal()
+        {
+            _car = new List<Car>
+            {
+                new Car{Id=1,CategoryId=1,BradnId=3,ColorId=90,DailyPrice=650,Description="Sedan Otomobil",ModelYear=2019},
+                new Car{Id=2,CategoryId=2,BradnId=7,ColorId=13,DailyPrice=800,Description="Hatcback Otomobil",ModelYear=2020},
+                new Car{Id=3,CategoryId=1,BradnId=6,ColorId=22,DailyPrice=750,Description="Coupe Otomobil",ModelYear=2022},
+                new Car{Id=4,CategoryId=2,BradnId=9,ColorId=33,DailyPrice=450,Description="Sedan Otomobil",ModelYear=2018},
+            };
+        }
+
+
+        public void Add(Car car)
+        {
+            _car.Add(car);
+            Console.WriteLine(car.Id + " - " + car.Description + " - " + car.ModelYear + " eklendi");
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _car.SingleOrDefault(p => p.Id == car.Id);
+            _car.Remove(carToDelete);
+            Console.WriteLine(carToDelete.Id + " - " + carToDelete.Description + " - " + carToDelete.ModelYear + " silindi");
+
+        }
+
+        public List<Car> GetAll()
+        {
+            return _car;
+        }
+
+        public List<Car> GetAllByCategory(int categoryId)
+        {
+            return _car.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _car.Where(p => p.Id == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car updateToCar = _car.SingleOrDefault(p => p.Id == car.Id);
+            updateToCar.DailyPrice = car.DailyPrice;
+            updateToCar.Description = car.Description;
+            updateToCar.CategoryId = car.CategoryId;
+            Console.WriteLine(car.Id + " - " + car.Description + " - " + car.DailyPrice + " - " + " güncellendi");
+        }
+    }
+}
